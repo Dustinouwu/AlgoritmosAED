e925b4f24ab5d39444756b1cbedaa958d5d9691d
Dustinouwu <74982150+Dustinouwu@users.noreply.github.com>
2021-03-15 22:51:54 -0500

    Add files via upload

---
+++ HEAPSORT/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include "Heapsort.h"
+using namespace std;
+void heapsort(float b[],int n);
+
+
+
+int main() {
+  ofstream registro;
+  registro.open("Heapsort”.txt",ios::app);
+  registro<<"SIN ORDENAR"<<endl;
+  float numerosuno[50];
+ int numeros;
+  cout<<"Ingrese el numero de elementos"<<endl;
+  cin>>numeros;
+  for(int i=1;i<=numeros;i++){
+    cout<<"Numeros"<<endl;
+    cin>>numerosuno[i];
+  }
+  for(int i=1;i<=numeros;i++){
+    cout<<"|"<<numerosuno[i];
+    registro<<"|"<<numerosuno[i]<<"|";
+  }
+  heapsort(numerosuno, numeros);
+  registro<<endl;
+  cout<<endl;
+  registro<<"HEAPSORT"<<endl;
+  cout<<"Ordenamiento"<<endl;
+
+  for(int i=1;i<=numeros;i++){
+    cout<<"|"<<numerosuno[i];
+    registro<<"|"<<numerosuno[i]<<"|";
+  }
+
+}
